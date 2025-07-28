a36d340e7ae5d07a29d4e2ebe610b6d039224410
krishna7275 <kpy.yt9833067464@gmail.com>
2025-07-28 20:02:34 +0530

    yooo

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-let todoArr =[];
+let todolist =['hello', 'world'];
+displayItem()
 
 function addTodo(){
-    let inputElement = document.querySelector('#todo-input')
+    let inputElement = document.querySelector('#todo-input');
+    let dateElement = document.querySelector('#todo-date');
     let todoItem = inputElement.value;
-    todoArr.push(todoItem);
+    todolist.push(todoItem);
     inputElement.value = '';
+    displayItem();
 
+}
+function displayItem(){
+let containerElement = document.querySelector('.todo-container');
+let newHtml = '';
+for(let i = 0; i < todolist.length; i++){
+    newHtml += `
+    <div>
+    <span>${todolist[i]}</span>
+    <button onclick="todolist.splice(${i},1);
+    displayItem();
+    ">Delete</button>
+    </div>
+    `
+}
+containerElement.innerHTML = newHtml;
 }
